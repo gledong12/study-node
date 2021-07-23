38f2086daeb1c4f61eb9b7864ab329714c463d0d
gledong12 <daniel.lee@photypeta.com>
2021-07-23 16:48:55 +0900

    express 시작
    Change-Id: I1e90ddfdb71b32b721b0ff00b1fb5e0e6c2a49f8

--- project/photo/test/backup.js
+++
@@ -1,9 +0,0 @@
-const path = require('paht');
-const fs = require('fs').promises;
-
-// 폴더 만들기
-fs.mkdir('video').catch(console.error);
-
-fs.mkdir('captured').catch(console.error);
-
-fs.mkdir('duplicated').catch(console.error);
